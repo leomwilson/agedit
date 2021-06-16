d710512b237766b1844cf102adf7f116b1579dad
Leo Wilson <leo@lwilson.dev>
2021-06-16 00:03:41 -0700

    Don't try to decrypt new files

--- agedit.sh
+++ agedit.sh
@@ -5,7 +5,7 @@ FNAME="/tmp/$(openssl rand -hex 32)"
 test -f $FNAME && exit 1
 touch $FNAME
 chmod 600 $FNAME
-age --decrypt -i ~/.config/agedit/identity.txt $1 > $FNAME
+test -f $1 && age --decrypt -i ~/.config/agedit/identity.txt $1 > $FNAME
 $EDITOR $FNAME
 age --encrypt -R ~/.config/agedit/recipients.txt $FNAME > $1
 rm $FNAME
